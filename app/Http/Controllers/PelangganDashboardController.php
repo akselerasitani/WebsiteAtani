fec19107a73589591f253b082bb684a8a827e8c8
tiararahmania05 <tiararahmania05@gmail.com>
2022-02-08 09:49:22 +0700

    struktur dan dashboard

---
+++ app/Http/Controllers/PelangganDashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PelangganDashboardController extends Controller
+{
+    public function index()
+    {
+        return view('Pelanggan.Dashboard.dashboard');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+use App\Http\Controllers\PelangganIndexController;
+use App\Http\Controllers\PelangganDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::prefix('/')->group(function () {
+    Route::prefix('/')->name('index.')->group(function () {
+        Route::get('/', [PelangganIndexController::class, 'index'])->name('index');
+    });
+
+    Route::prefix('/dashboard')->name('dashboard.')->group(function () {
+        Route::get('/', [PelangganDashboardController::class, 'index'])->name('index');
+    });
+}); 
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::get('/registertoko', [AuthController::class, 'registertoko']);
+Route::get('/suksesregister', [AuthController::class, 'suksesregister']);
